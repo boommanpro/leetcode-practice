4a7262e7ee19f8b294cfec1385e6f431f29b0f50
BoomManPro <l.o.v.e_y.o.u_3344@qq.com>
2019-03-14 22:12:31 +0800

    leetcode718 commit

---
+++ src/test/java/string/Leetcode718Test.java
@@ -0,0 +1,41 @@
+package string;
+
+import org.junit.Test;
+
+public class Leetcode718Test {
+    @Test
+    public void leetcode718Test(){
+
+        //     Input:
+        //     A: [1,2,3,2,1]
+        //     B: [3,2,1,4,7]
+        //     Output: 3
+        //     Explanation:
+        //     The repeated subarray with maximum length is [3, 2, 1].
+
+
+        //    [0,1,1,1,1]
+        //    [1,0,1,0,1]
+        int length = findLength(new int[]{1,2,3,2,1}, new int[]{3,2,1,4,7});
+
+        System.out.println(String.format("result:%s",length));
+    }
+
+
+    public int findLength(int[] A, int[] B) {
+        int m = A.length;
+        int n = B.length;
+
+        int[][] dp = new int[m + 1][n + 1];
+        int maxLength = 0;
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (A[i - 1] == B[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                    maxLength = Math.max(dp[i][j], maxLength);
+                }
+            }
+        }
+        return maxLength;
+    }
+}
