52a2625130a7e647b5fb9fc6c08325db3466eb02
wangqimeng <wangqimeng@mininglamp.com>
2019-05-21 18:47:21 +0800

    update leetcode31

---
+++ src/test/java/sort/Leetcode31Test.java
@@ -0,0 +1,33 @@
+package sort;
+
+import org.junit.Test;
+
+public class Leetcode31Test {
+
+
+    @Test
+    public void leetcode31Test() {
+
+    }
+
+    /**
+     * from:https://leetcode.com/problems/next-permutation/
+     * ch-from:https://leetcode-cn.com/problems/next-permutation/
+     *
+     * Implement next permutation, which rearranges numbers into the lexicographically next greater permutation of numbers.
+     * <p>
+     * If such arrangement is not possible, it must rearrange it as the lowest possible order (ie, sorted in ascending order).
+     * <p>
+     * The replacement must be in-place and use only constant extra memory.
+     * <p>
+     * Here are some examples. Inputs are in the left-hand column and its corresponding outputs are in the right-hand column.
+     * <p>
+     * 1,2,3 → 1,3,2
+     * 3,2,1 → 1,2,3
+     * 1,1,5 → 1,5,1
+     *
+     */
+    public void nextPermutation(int[] nums) {
+
+    }
+}
