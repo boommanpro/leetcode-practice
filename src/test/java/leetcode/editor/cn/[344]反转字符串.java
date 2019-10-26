1a345eab8c1b1549027b6e1c69e184e9bb6f7f92
BoomManPro <boommanpro@gmail.com>
2019-10-26 18:14:21 +0800

    solve 344 反转字符串

---
+++ src/test/java/leetcode/editor/cn/[344]反转字符串.java
@@ -0,0 +1,62 @@
+package leetcode.editor.cn;
+
+import org.junit.Assert;
+import org.junit.Test;
+class SolutionTest344 {
+//编写一个函数，其作用是将输入的字符串反转过来。输入字符串以字符数组 char[] 的形式给出。 
+//
+// 不要给另外的数组分配额外的空间，你必须原地修改输入数组、使用 O(1) 的额外空间解决这一问题。 
+//
+// 你可以假设数组中的所有字符都是 ASCII 码表中的可打印字符。 
+//
+// 
+//
+// 示例 1： 
+//
+// 输入：['h','e','l','l','o']
+//输出：['o','l','l','e','h']
+// 
+//
+// 示例 2： 
+//
+// 输入：['H','a','n','n','a','h']
+//输出：['h','a','n','n','a','H']
+// Related Topics 双指针 字符串
+    public static
+    
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public void reverseString(char[] s) {
+        int l = 0, r = s.length - 1;
+        while (l < r) {
+            swap(s,l,r);
+            l++;
+            r--;
+        }
+    }
+
+    private void swap(char[] s, int a, int b) {
+        char c = s[a];
+        s[a] = s[b];
+        s[b] = c;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+//Do some Test
+    public static class TestClass {
+
+        @Test
+        public void defaultSolutionTest() {
+            Solution solution = new Solution();
+            char[] array1 = new char[]{'h', 'e', 'l', 'l', 'o'};
+            solution.reverseString(array1);
+            Assert.assertArrayEquals(new char[]{'o','l','l','e','h'}, array1);
+
+            char[] array2 = new char[]{'H','a','n','n','a','h'};
+            solution.reverseString(array2);
+            Assert.assertArrayEquals(new char[]{'h','a','n','n','a','H'}, array2);
+        }
+    }
+}
