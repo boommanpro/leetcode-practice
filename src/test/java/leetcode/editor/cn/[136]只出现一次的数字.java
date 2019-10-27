4832ff15c798e56c8991d41280b65ed128c0a0e1
BoomManPro <boommanpro@gmail.com>
2019-10-27 16:27:29 +0800

    solve 136 只出现一次的数字

---
+++ src/test/java/leetcode/editor/cn/[136]只出现一次的数字.java
@@ -0,0 +1,52 @@
+package leetcode.editor.cn;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class SolutionTest136 {
+    //给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
+//
+// 说明： 
+//
+// 你的算法应该具有线性时间复杂度。 你可以不使用额外空间来实现吗？ 
+//
+// 示例 1: 
+//
+// 输入: [2,2,1]
+//输出: 1
+// 
+//
+// 示例 2: 
+//
+// 输入: [4,1,2,1,2]
+//输出: 4 
+// Related Topics 位运算 哈希表
+    public static
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int singleNumber(int[] nums) {
+            int result = 0;
+            for (int num : nums) {
+                result ^= num;
+            }
+            return result;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+    //Do some Test
+    public static class TestClass {
+
+        @Test
+        public void defaultSolutionTest() {
+            Solution solution = new Solution();
+            Assert.assertEquals(1, solution.singleNumber(new int[]{2, 2, 1}));
+            Assert.assertEquals(4, solution.singleNumber(new int[]{4, 1, 2, 1, 2}));
+        }
+    }
+}
