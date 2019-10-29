c135b27ef0f29efab6def6c225d91cbd5fd00f18
wangqimeng <wangqimeng@mininglamp.com>
2019-10-29 09:29:00 +0800

    solve 21 合并两个有序链表

---
+++ src/test/java/leetcode/editor/cn/[21]合并两个有序链表.java
@@ -0,0 +1,71 @@
+package leetcode.editor.cn;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+class SolutionTest21 {
+
+    //将两个有序链表合并为一个新的有序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+//
+// 示例： 
+//
+// 输入：1->2->4, 1->3->4
+//输出：1->1->2->3->4->4
+// 
+// Related Topics 链表
+    public static
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+    class Solution {
+
+        public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+            ListNode dump = new ListNode(-1);
+            ListNode curr = dump;
+            while (l1 != null || l2 != null) {
+                if (l2 == null) {
+                    curr.next = l1;
+                    break;
+                }
+                if (l1 == null ) {
+                    curr.next = l2;
+                    break;
+                }
+                if (l1.val <= l2.val) {
+                    curr.next = new ListNode(l1.val);
+                    l1 = l1.next;
+                    curr = curr.next;
+                }else {
+                    curr.next = new ListNode(l2.val);
+                    l2 = l2.next;
+                    curr = curr.next;
+                }
+            }
+            return dump.next;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+    //Do some Test
+    public static class TestClass {
+
+        @Test
+        public void defaultSolutionTest() {
+            Solution solution = new Solution();
+            ListNode node1 = new ListNode(1);
+            node1.addNext(2).addNext(4);
+
+            ListNode node2 = new ListNode(1);
+            node2.addNext(3).addNext(4);
+            Assert.assertEquals("112344", solution.mergeTwoLists(node1, node2).getPositiveListNodeValue());
+        }
+    }
+}
