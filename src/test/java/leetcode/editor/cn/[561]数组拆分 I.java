8fda0d82911a1c377aea95041cea41e5f43a2009
boommanpro <boommanpro@gmail.com>
2020-06-22 22:31:39 +0800

    feat(solve [561] 数组拆分 I): 数组拆分 先排序 然后取前面那个

---
+++ src/test/java/leetcode/editor/cn/[561]数组拆分 I.java
@@ -0,0 +1,56 @@
+package leetcode.editor.cn;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+class SolutionTest561 {
+//给定长度为 2n 的数组, 你的任务是将这些数分成 n 对, 例如 (a1, b1), (a2, b2), ..., (an, bn) ，使得从1 到 n 
+//的 min(ai, bi) 总和最大。 
+//
+// 示例 1: 
+//
+// 
+//输入: [1,4,3,2]
+//
+//输出: 4
+//解释: n 等于 2, 最大总和为 4 = min(1, 2) + min(3, 4).
+// 
+//
+// 提示: 
+//
+// 
+// n 是正整数,范围在 [1, 10000]. 
+// 数组中的元素范围在 [-10000, 10000]. 
+// 
+// Related Topics 数组
+
+    public static
+            //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int arrayPairSum(int[] nums) {
+            if (nums == null || nums.length == 0) {
+                return 0;
+            }
+            Arrays.sort(nums);
+            int n = nums.length;
+            int sum = 0;
+            for (int i = 0; i < n; i+=2) {
+                sum += nums[i];
+            }
+            return sum;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+    //Do some Test
+    public static class TestClass {
+
+        @Test
+        public void defaultSolutionTest() {
+            Solution solution = new Solution();
+            Assert.assertEquals(4, solution.arrayPairSum(new int[]{1, 4, 3, 2}));
+        }
+    }
+}
