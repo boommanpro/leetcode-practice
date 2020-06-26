3e75fe513095a3e9852bb3c8a1a6ea10c26814f6
boommanpro <boommanpro@gmail.com>
2020-06-26 12:59:22 +0800

    feat(solve [700]二叉搜索树中的搜索): 递归分之查找

---
+++ src/test/java/leetcode/editor/cn/[700]二叉搜索树中的搜索.java
@@ -0,0 +1,70 @@
+package leetcode.editor.cn;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+class SolutionTest700 {
+//给定二叉搜索树（BST）的根节点和一个值。 你需要在BST中找到节点值等于给定值的节点。 返回以该节点为根的子树。 如果节点不存在，则返回 NULL。 
+//
+// 例如， 
+//
+// 
+//给定二叉搜索树:
+//
+//        4
+//       / \
+//      2   7
+//     / \
+//    1   3
+//
+//和值: 2
+// 
+//
+// 你应该返回如下子树: 
+//
+// 
+//      2     
+//     / \   
+//    1   3
+// 
+//
+// 在上述示例中，如果要找的值是 5，但因为没有节点值为 5，我们应该返回 NULL。 
+// Related Topics 树
+
+    public static
+            //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+    class Solution {
+        public TreeNode searchBST(TreeNode root, int val) {
+            if (root == null) {
+                return null;
+            }
+            if (root.val == val) {
+                return root;
+            }
+            if (root.val > val) {
+                return searchBST(root.left, val);
+            }
+            return searchBST(root.right, val);
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)a
+
+    //Do some Test
+    public static class TestClass {
+
+        @Test
+        public void defaultSolutionTest() {
+            Solution solution = new Solution();
+            Assert.assertEquals("[2, 1, 3]", solution.searchBST(TreeNode.getTreeNode(new Integer[]{4, 2, 7, 1, 3}), 2).toIntArrayString());
+        }
+    }
+}
