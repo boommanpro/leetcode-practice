474a8debc33e8889c649f1f4230c8a1dc13d37de
boommanpro <boommanpro@gmail.com>
2020-06-30 22:37:24 +0800

    feat(solve [面试题 02.03] 删除中间节点): tag: 链表
    其实你是没办法删除的,所以你只能平移数据,删除末尾节点

---
+++ src/test/java/leetcode/editor/cn/[面试题 02.03]删除中间节点.java
@@ -0,0 +1,50 @@
+package leetcode.editor.cn;
+
+import org.junit.Test;
+
+class SolutionTest面试题02_03 {
+//实现一种算法，删除单向链表中间的某个节点（即不是第一个或最后一个节点），假定你只能访问该节点。 
+//
+// 
+//
+// 示例： 
+//
+// 输入：单向链表a->b->c->d->e->f中的节点c
+//结果：不返回任何数据，但该链表变为a->b->d->e->f
+// 
+// Related Topics 链表
+
+    public static
+            //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+    class Solution {
+        public void deleteNode(ListNode node) {
+            while (node.next != null) {
+                node.val = node.next.val;
+                if (node.next.next == null) {
+                    node.next = null;
+                    return;
+                }
+                node = node.next;
+            }
+
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+    //Do some Test
+    public static class TestClass {
+
+        @Test
+        public void defaultSolutionTest() {
+            Solution solution = new Solution();
+        }
+    }
+}
