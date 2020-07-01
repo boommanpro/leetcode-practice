b1178ed8aa7a87ca715aad73d605e6e8fb6ba40d
boommanpro <boommanpro@gmail.com>
2020-07-01 14:32:02 +0800

    feat(solve [面试题 08.06] 汉诺塔问题): tag: 递归 栈
    使用栈解决汉诺塔问题
    注意点:
    1. 是使用栈的操作
    2. 递归解决
    3. 多画图分解 把问题简单化可能就出来了

--- src/test/java/leetcode/editor/cn/[面试题 08.06]汉诺塔问题.java
+++ src/test/java/leetcode/editor/cn/[面试题 08.06]汉诺塔问题.java
@@ -41,21 +41,35 @@ class SolutionTest面试题08_06 {
     public static
             //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
+
+        private List<Integer> watchA;
+
+        private List<Integer> watchB;
+
+        private List<Integer> watchC;
+
         public void hanota(List<Integer> A, List<Integer> B, List<Integer> C) {
-            if (A == null || B == null || C == null) {
-                return;
-            }
-            move(A.size(), A, B, C);
+            watchA = A;
+            watchB = B;
+            watchC = C;
+            int n = A.size();
+            move(n, A, B, C);
         }
 
         private void move(int n, List<Integer> A, List<Integer> B, List<Integer> C) {
-            if (n < 1) {
+            //我们将汉诺塔问题分割成 n-1个顶部和 1个底部的递归调用问题
+            //因为只能使用栈的思想所以
+            //所以我们操作list要像操作栈一样
+            //后进先出
+            // 每次pop最小的元素 即-> A.remove(A.size-1)
+            // 每次push 进入就是A.add()
+            if (n == 1) {
+                C.add(A.remove(A.size() - 1));
                 return;
             }
+            //如果n=2的话  需要先把A的0先移动到B  然后把A的1移动到C 再把B移动到C 递归就如下了
             move(n - 1, A, C, B);
-            Integer num = A.get(A.size() - 1);
-            A.remove(num);
-            C.add(num);
+            C.add(A.remove(A.size() - 1));
             move(n - 1, B, A, C);
         }
     }
@@ -67,15 +81,24 @@ class SolutionTest面试题08_06 {
         @Test
         public void defaultSolutionTest() {
             Solution solution = new Solution();
+            List<Integer> C0 = new ArrayList<>();
+            solution.hanota(new ArrayList<>(Arrays.asList(0)), new ArrayList<>(), C0);
+            Assert.assertEquals("[0]", C0.toString());
             List<Integer> C1 = new ArrayList<>();
-            solution.hanota(new ArrayList<>(Arrays.asList(2, 1, 0)), new ArrayList<>(), C1);
-            Assert.assertEquals("[2, 1, 0]", C1.toString());
+            solution.hanota(new ArrayList<>(Arrays.asList(1, 0)), new ArrayList<>(), C1);
+            Assert.assertEquals("[1, 0]", C1.toString());
             List<Integer> C2 = new ArrayList<>();
-            solution.hanota(new ArrayList<>(Arrays.asList(1, 0)), new ArrayList<>(), C2);
-            Assert.assertEquals("[1, 0]", C2.toString());
+            solution.hanota(new ArrayList<>(Arrays.asList(2, 1, 0)), new ArrayList<>(), C2);
+            Assert.assertEquals("[2, 1, 0]", C2.toString());
             List<Integer> C3 = new ArrayList<>();
-            solution.hanota(new ArrayList<>(Arrays.asList(5, 4, 3, 2, 1, 0)), new ArrayList<>(), C3);
-            Assert.assertEquals("[5, 4, 3, 2, 1, 0]", C3.toString());
+            solution.hanota(new ArrayList<>(Arrays.asList(3, 2, 1, 0)), new ArrayList<>(), C3);
+            Assert.assertEquals("[3, 2, 1, 0]", C3.toString());
+            List<Integer> C4 = new ArrayList<>();
+            solution.hanota(new ArrayList<>(Arrays.asList(4, 3, 2, 1, 0)), new ArrayList<>(), C4);
+            Assert.assertEquals("[4, 3, 2, 1, 0]", C4.toString());
+            List<Integer> C5 = new ArrayList<>();
+            solution.hanota(new ArrayList<>(Arrays.asList(5, 4, 3, 2, 1, 0)), new ArrayList<>(), C5);
+            Assert.assertEquals("[5, 4, 3, 2, 1, 0]", C5.toString());
         }
     }
 }
