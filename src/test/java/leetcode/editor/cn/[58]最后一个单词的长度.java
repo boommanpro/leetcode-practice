2d2cef6de2f1266168c64dbc604449a674b505bf
boommanpro <boommanpro@gmail.com>
2020-07-14 20:15:56 +0800

    feat(solve [58] 最后一个单词的长度): tag: easy more-practice

---
+++ src/test/java/leetcode/editor/cn/[58]最后一个单词的长度.java
@@ -0,0 +1,56 @@
+package leetcode.editor.cn;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+class SolutionTest58 {
+//给定一个仅包含大小写字母和空格 ' ' 的字符串 s，返回其最后一个单词的长度。如果字符串从左向右滚动显示，那么最后一个单词就是最后出现的单词。 
+//
+// 如果不存在最后一个单词，请返回 0 。 
+//
+// 说明：一个单词是指仅由字母组成、不包含任何空格字符的 最大子字符串。 
+//
+// 
+//
+// 示例: 
+//
+// 输入: "Hello World"
+//输出: 5
+// 
+// Related Topics 字符串 
+// 👍 215 👎 0
+
+    public static
+            //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+
+        public int lengthOfLastWord(String s) {
+            if (s == null || s.length() == 0) return 0;
+            int count = 0;
+            for (int i = s.length() - 1; i >= 0; i--) {
+                if (s.charAt(i) == ' ') {
+                    if (count == 0) {
+                        continue;
+                    }
+                    break;
+                }
+                count++;
+            }
+            return count;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+    //Do some Test
+    public static class TestClass {
+
+        @Test
+        public void defaultSolutionTest() {
+            Solution solution = new Solution();
+            Assert.assertEquals(0, solution.lengthOfLastWord(""));
+            Assert.assertEquals(5, solution.lengthOfLastWord("Hello World"));
+            Assert.assertEquals(1, solution.lengthOfLastWord("Hello s s Worldabcs s"));
+            Assert.assertEquals(1, solution.lengthOfLastWord("Hello s s Worldabcs s   "));
+        }
+    }
+}
