c0d4d53d908970425dd2bebc800310b894965b03
boommanpro <boommanpro@gmail.com>
2020-07-15 17:09:07 +0800

    feat(solve [107] 二叉树的层序遍历 II): tag: tree

---
+++ src/test/java/leetcode/editor/cn/[107]二叉树的层次遍历 II.java
@@ -0,0 +1,86 @@
+package leetcode.editor.cn;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+class SolutionTest107 {
+//给定一个二叉树，返回其节点值自底向上的层次遍历。 （即按从叶子节点所在层到根节点所在的层，逐层从左向右遍历） 
+//
+// 例如： 
+//给定二叉树 [3,9,20,null,null,15,7], 
+//
+//     3
+//   / \
+//  9  20
+//    /  \
+//   15   7
+// 
+//
+// 返回其自底向上的层次遍历为： 
+//
+// [
+//  [15,7],
+//  [9,20],
+//  [3]
+//]
+// 
+// Related Topics 树 广度优先搜索 
+// 👍 264 👎 0
+
+    public static
+            //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+    class Solution {
+
+        public List<List<Integer>> levelOrderBottom(TreeNode root) {
+            if (root == null) {
+                return new ArrayList<>();
+            }
+            LinkedList<List<Integer>> result = new LinkedList<>();
+            Queue<TreeNode> queue = new LinkedList<>();
+            queue.offer(root);
+            while (!queue.isEmpty()) {
+                int n = queue.size();
+                List<Integer> subList = new ArrayList<>();
+                while (n > 0) {
+                    TreeNode node = queue.poll();
+                    if (node.left != null) {
+                        queue.offer(node.left);
+                    }
+                    if (node.right != null) {
+                        queue.offer(node.right);
+                    }
+                    subList.add(node.val);
+                    n--;
+                }
+                result.addFirst(subList);
+            }
+            return result;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+    //Do some Test
+    public static class TestClass {
+
+        @Test
+        public void defaultSolutionTest() {
+            Solution solution = new Solution();
+            Assert.assertEquals("[[15, 7], [9, 20], [3]]", solution.levelOrderBottom(TreeNode.getTreeNode(new Integer[]{3, 9, 20, null, null, 15, 7})).toString());
+
+        }
+    }
+}
