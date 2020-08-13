02eb2f26036b72e922844cf25ae8b7bd21b9fdfc
boommanpro <boommanpro@gmail.com>
2020-08-13 19:48:02 +0800

    feat(solve [面试题 17.22]单词转换): tag: dfs

--- src/test/java/leetcode/editor/cn/[面试题 17.22]单词转换.java
+++ src/test/java/leetcode/editor/cn/[面试题 17.22]单词转换.java
@@ -3,7 +3,12 @@ package leetcode.editor.cn;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 class SolutionTest面试题_17_22 {
 //给定字典中的两个词，长度相等。写一个方法，把一个词转换成另一个词， 但是一次只能改变一个字符。每一步得到的新词都必须能在字典中找到。 
@@ -37,18 +42,58 @@ class SolutionTest面试题_17_22 {
     public static
             //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
+
+
         public List<String> findLadders(String beginWord, String endWord, List<String> wordList) {
-            //每次只能转换一个单词 并且这个单词要在wordList中找到
-            //endWord要在wordList中...
-
-            List<String> result = new ArrayList<>();
-            Set<String> wordSet = new HashSet<>(wordList);
-            if (!wordSet.contains(endWord)) {
-                return result;
+            Map<String, Boolean> wordMap = wordList.stream().collect(Collectors.toMap(Function.identity(), s -> false));
+            if (!wordMap.containsKey(endWord)) {
+                return new ArrayList<>();
             }
-
-            return Arrays.asList("hit","hot","dot","lot","log","cog");
+            List<String> result = new ArrayList<>();
+            dfs(beginWord, endWord, new ArrayList<>(), wordMap, result);
+            return result;
         }
+
+        private void dfs(String beginWord, String endWord, List<String> path, Map<String, Boolean> selectPath, List<String> result) {
+            path.add(beginWord);
+            for (String tempWord : getNextWords(beginWord, selectPath)) {
+                if (tempWord.equals(endWord)) {
+                    path.add(tempWord);
+                    result.addAll(path);
+                    return;
+                }
+                dfs(tempWord, endWord, path, selectPath, result);
+                path.remove(path.size() - 1);
+            }
+        }
+
+        private List<String> getNextWords(String beginWord, Map<String, Boolean> selectPath) {
+            List<String> result = new ArrayList<>();
+            for (Map.Entry<String, Boolean> entry : selectPath.entrySet()) {
+                if (!entry.getValue()) {
+                    if (compareDiff(beginWord, entry.getKey()) == 1) {
+                        result.add(entry.getKey());
+                        entry.setValue(true);
+                    }
+                }
+            }
+            return result;
+        }
+
+        private int compareDiff(String beginWord, String key) {
+            int count = 0;
+            int n = beginWord.length();
+            for (int i = 0; i < n; i++) {
+                if (beginWord.charAt(i) != key.charAt(i)) {
+                    count++;
+                }
+                if (count > 1) {
+                    return Integer.MAX_VALUE;
+                }
+            }
+            return count;
+        }
+
     }
 //leetcode submit region end(Prohibit modification and deletion)
 
@@ -58,7 +103,7 @@ class SolutionTest面试题_17_22 {
         @Test
         public void defaultSolutionTest() {
             Solution solution = new Solution();
-            Assert.assertEquals("[hit, hot, dot, lot, log, cog]", solution.findLadders("hit", "cog", Arrays.asList("hot", "dot", "dog", "lot", "log", "cog")).toString());
+            Assert.assertEquals("[hit, hot, lot, log, cog]", solution.findLadders("hit", "cog", new ArrayList<>(Arrays.asList("hot", "dot", "dog", "lot", "log", "cog"))).toString());
             Assert.assertEquals("[]", solution.findLadders("hit", "cog", Arrays.asList("hot", "dot", "dog", "lot", "log")).toString());
         }
     }
