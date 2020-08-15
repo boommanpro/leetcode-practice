019aa85c4b11fefa808ed3540917257ca917cacd
boommanpro <boommanpro@gmail.com>
2020-08-15 12:34:40 +0800

    feat(solve [546] 移除盒子): tag: dp more-practice

--- src/test/java/leetcode/editor/cn/[546]移除盒子.java
+++ src/test/java/leetcode/editor/cn/[546]移除盒子.java
@@ -1,9 +1,5 @@
 package leetcode.editor.cn;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -42,82 +38,25 @@ class SolutionTest546 {
             //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
 
-        private int score = 0;
-
         public int removeBoxes(int[] boxes) {
-            score = 0;
-            if (boxes == null || boxes.length == 0) {
-                return 0;
-            }
-            List<Integer> selectPath = Arrays.stream(boxes).boxed().collect(Collectors.toList());
-            backtracking(selectPath, 0);
-            return score;
+            int[][][] dp = new int[100][100][100];
+            return calculatePoints(boxes, dp, 0, boxes.length - 1, 0);
         }
 
-        private void backtracking(List<Integer> selectPath, int curr) {
-            if (selectPath.size() == 0) {
-                score = Math.max(curr, score);
-                return;
+        public int calculatePoints(int[] boxes, int[][][] dp, int l, int r, int k) {
+            if (l > r) return 0;
+            if (dp[l][r][k] != 0) return dp[l][r][k];
+            while (r > l && boxes[r] == boxes[r - 1]) {
+                r--;
+                k++;
             }
-            int n = selectPath.size();
-            for (int i = 0; i < n; i++) {
-                Sequence sequence = getSequence(selectPath, i);
-                i = sequence.end;
-                //删除
-                delete(selectPath, sequence);
-                backtracking(selectPath, curr + sequence.length * sequence.length);
-                //恢复
-                recovery(selectPath, sequence);
-            }
-        }
-
-        private void recovery(List<Integer> selectPath, Sequence sequence) {
-            int len = sequence.length;
-            int start = sequence.start;
-            int value = sequence.value;
-            while (len > 0) {
-                selectPath.add(start, value);
-                len--;
-            }
-        }
-
-        private void delete(List<Integer> selectPath, Sequence sequence) {
-            int start = sequence.start;
-            int len = sequence.length;
-            while (len > 0) {
-                selectPath.remove(start);
-                len--;
-            }
-        }
-
-        private Sequence getSequence(List<Integer> selectPath, int start) {
-            int n = selectPath.size();
-            int end = start;
-            for (int i = start + 1; i < n; i++) {
-                if (!selectPath.get(i).equals(selectPath.get(start))) {
-                    break;
+            dp[l][r][k] = calculatePoints(boxes, dp, l, r - 1, 0) + (k + 1) * (k + 1);
+            for (int i = l; i < r; i++) {
+                if (boxes[i] == boxes[r]) {
+                    dp[l][r][k] = Math.max(dp[l][r][k], calculatePoints(boxes, dp, l, i, k + 1) + calculatePoints(boxes, dp, i + 1, r - 1, 0));
                 }
-                end = i;
             }
-            return new Sequence(start, end, selectPath.get(start));
-        }
-
-        public class Sequence {
-
-            int start;
-
-            int end;
-
-            int length;
-
-            int value;
-
-            public Sequence(int start, int end, int value) {
-                this.start = start;
-                this.end = end;
-                this.value = value;
-                this.length = end - start + 1;
-            }
+            return dp[l][r][k];
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
@@ -129,7 +68,7 @@ class SolutionTest546 {
         public void defaultSolutionTest() {
             Solution solution = new Solution();
             Assert.assertEquals(23, solution.removeBoxes(new int[]{1, 3, 2, 2, 2, 3, 4, 3, 1}));
-            Assert.assertEquals(1, solution.removeBoxes(new int[]{3, 8, 8, 5, 5, 3, 9, 2, 4, 4, 6, 5, 8, 4, 8, 6, 9, 6, 2, 8, 6, 4, 1, 9, 5, 3, 10, 5, 3, 3, 9, 8, 8, 6, 5, 3, 7, 4, 9, 6, 3, 9, 4, 3, 5, 10, 7, 6, 10, 7}));
+//            Assert.assertEquals(136, solution.removeBoxes(new int[]{3, 8, 8, 5, 5, 3, 9, 2, 4, 4, 6, 5, 8, 4, 8, 6, 9, 6, 2, 8, 6, 4, 1, 9, 5, 3, 10, 5, 3, 3, 9, 8, 8, 6, 5, 3, 7, 4, 9, 6, 3, 9, 4, 3, 5, 10, 7, 6, 10, 7}));
         }
     }
 }
