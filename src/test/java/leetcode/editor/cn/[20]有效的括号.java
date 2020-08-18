14d3791e645aafd8f5a6cb04854a3919fa1decb0
boommanpro <boommanpro@gmail.com>
2020-08-18 09:37:25 +0800

    feat(test [20] 有效的括号): add some test

--- src/test/java/leetcode/editor/cn/[20]有效的括号.java
+++ src/test/java/leetcode/editor/cn/[20]有效的括号.java
@@ -98,6 +98,7 @@ class SolutionTest20 {
             Assert.assertFalse(solution.isValid("{[]}{"));
             Assert.assertFalse(solution.isValid("(]"));
             Assert.assertFalse(solution.isValid("([)]"));
+            Assert.assertFalse(solution.isValid("]"));
         }
     }
 }
