fad36c1ec0349a1a19083d17f4fb63e0341af4d4
boommanpro <boommanpro@gmail.com>
2020-08-21 11:17:34 +0800

    feat(solve [103] 二叉树的锯齿形层序遍历): tag: 二叉树遍历 bfs

---
+++ src/test/java/leetcode/editor/cn/[103]二叉树的锯齿形层次遍历.java
@@ -0,0 +1,93 @@
+package leetcode.editor.cn;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+class SolutionTest103 {
+//给定一个二叉树，返回其节点值的锯齿形层次遍历。（即先从左往右，再从右往左进行下一层遍历，以此类推，层与层之间交替进行）。 
+//
+// 例如： 
+//给定二叉树 [3,9,20,null,null,15,7], 
+//
+//     3
+//   / \
+//  9  20
+//    /  \
+//   15   7
+// 
+//
+// 返回锯齿形层次遍历如下： 
+//
+// [
+//  [3],
+//  [20,9],
+//  [15,7]
+//]
+// 
+// Related Topics 栈 树 广度优先搜索 
+// 👍 252 👎 0
+
+    public static
+            //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+    class Solution {
+
+        @SuppressWarnings("all")
+        public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+            List<List<Integer>> result = new ArrayList<>();
+            if (root == null) {
+                return result;
+            }
+            Queue<TreeNode> queue = new LinkedList<>();
+            queue.offer(root);
+            boolean reverse = false;
+            while (!queue.isEmpty()) {
+                int n = queue.size();
+                List<Integer> curr = new ArrayList<>();
+                while (n > 0) {
+                    TreeNode node = queue.poll();
+                    curr.add(node.val);
+                    if (node.left != null) {
+                        queue.offer(node.left);
+                    }
+                    if (node.right != null) {
+                        queue.offer(node.right);
+                    }
+                    n--;
+                }
+                if (reverse) {
+                    Collections.reverse(curr);
+                }
+                result.add(curr);
+                reverse = !reverse;
+            }
+            return result;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+    //Do some Test
+    public static class TestClass {
+
+        @Test
+        public void defaultSolutionTest() {
+            Solution solution = new Solution();
+            Assert.assertEquals("[[3], [20, 9], [15, 7]]", solution.zigzagLevelOrder(TreeNode.getTreeNode(new Integer[]{3, 9, 20, null, null, 15, 7})).toString());
+            Assert.assertEquals("[]", solution.zigzagLevelOrder(null).toString());
+        }
+    }
+}
