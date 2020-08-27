7b70cb40e56f4392cf82efe725ccd3421ac857e3
boommanpro <boommanpro@gmail.com>
2020-08-27 21:34:04 +0800

    feat(solve [213] 打家劫舍 II): tag: dp

---
+++ src/test/java/leetcode/editor/cn/[213]打家劫舍 II.java
@@ -0,0 +1,68 @@
+package leetcode.editor.cn;
+
+import leetcode.editor.cn.utils.ArrayUtils;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+class SolutionTest213 {
+//你是一个专业的小偷，计划偷窃沿街的房屋，每间房内都藏有一定的现金。这个地方所有的房屋都围成一圈，这意味着第一个房屋和最后一个房屋是紧挨着的。同时，相邻的房屋
+//装有相互连通的防盗系统，如果两间相邻的房屋在同一晚上被小偷闯入，系统会自动报警。 
+//
+// 给定一个代表每个房屋存放金额的非负整数数组，计算你在不触动警报装置的情况下，能够偷窃到的最高金额。 
+//
+// 示例 1: 
+//
+// 输入: [2,3,2]
+//输出: 3
+//解释: 你不能先偷窃 1 号房屋（金额 = 2），然后偷窃 3 号房屋（金额 = 2）, 因为他们是相邻的。
+// 
+//
+// 示例 2: 
+//
+// 输入: [1,2,3,1]
+//输出: 4
+//解释: 你可以先偷窃 1 号房屋（金额 = 1），然后偷窃 3 号房屋（金额 = 3）。
+//     偷窃到的最高金额 = 1 + 3 = 4 。 
+// Related Topics 动态规划 
+// 👍 358 👎 0
+
+    public static
+            //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int rob(int[] nums) {
+            if (nums == null || nums.length == 0) {
+                return 0;
+            }
+            int n = nums.length;
+            if (n == 1) {
+                return nums[0];
+            }
+            return Math.max(myRob(Arrays.copyOfRange(nums, 1, n)), myRob(Arrays.copyOfRange(nums, 0, n - 1)));
+        }
+
+        private int myRob(int[] nums) {
+            int pre = 0, curr = 0, temp;
+            for (int num : nums) {
+                temp = curr;
+                curr = Math.max(pre + num, curr);
+                pre = temp;
+            }
+            return curr;
+        }
+
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+    //Do some Test
+    public static class TestClass {
+
+        @Test
+        public void defaultSolutionTest() {
+            Solution solution = new Solution();
+            Assert.assertEquals(3, solution.rob(new int[]{2, 3, 2}));
+            Assert.assertEquals(4, solution.rob(new int[]{1, 2, 3, 1}));
+        }
+    }
+}
