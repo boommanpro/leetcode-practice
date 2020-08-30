3541304621bee537fb465601192021db667d4e22
boommanpro <boommanpro@gmail.com>
2020-08-30 16:25:42 +0800

    feat(solve [712] 两个字符串的最小ASCII删除和): tag: dp

--- src/test/java/leetcode/editor/cn/[712]两个字符串的最小ASCII删除和.java
+++ src/test/java/leetcode/editor/cn/[712]两个字符串的最小ASCII删除和.java
@@ -39,8 +39,27 @@ class SolutionTest712 {
     public static
             //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
+
         public int minimumDeleteSum(String s1, String s2) {
-            return 0;
+            int m = s1.length();
+            int n = s2.length();
+            int[][] f = new int[m + 1][n + 1];
+            for (int i = 1; i <= m; i++) {
+                f[i][0] = f[i - 1][0] + s1.codePointAt(i - 1);
+            }
+            for (int i = 1; i <= n; i++) {
+                f[0][i] = f[0][i - 1] + s2.codePointAt(i - 1);
+            }
+            for (int i = 1; i <= m; i++) {
+                for (int j = 1; j <= n; j++) {
+                    if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                        f[i][j] = f[i - 1][j - 1];
+                    } else {
+                        f[i][j] = Math.min(f[i - 1][j] + s1.codePointAt(i - 1), f[i][j - 1] + s2.codePointAt(j - 1));
+                    }
+                }
+            }
+            return f[m][n];
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
