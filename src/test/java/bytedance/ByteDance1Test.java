47d93f00cffc86b4ae66e64a75b7f96aac69652d
boommanpro <boommanpro@gmail.com>
2020-09-07 13:39:07 +0800

    feat(add problem):

---
+++ src/test/java/bytedance/ByteDance1Test.java
@@ -0,0 +1,33 @@
+package bytedance;
+
+import org.junit.Test;
+
+/**
+ * @author wangqimeng
+ * @date 2020/9/6 16:10
+ */
+public class ByteDance1Test {
+
+
+    // 1.程序随机生成100个长度在100到200位的长整数
+    // 2.对这些长整数累加求和，并输出sum
+
+    public static
+    class Solution {
+
+        public int fun() {
+
+            return 0;
+        }
+    }
+
+    public static class TestClass {
+
+        @Test
+        public void defaultSolutionTest() {
+            Solution solution = new Solution();
+
+        }
+    }
+
+}
