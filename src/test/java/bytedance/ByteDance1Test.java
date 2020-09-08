3a681ec43580a73c76793985de3c60fcd44b8001
boommanpro <boommanpro@gmail.com>
2020-09-08 11:20:30 +0800

    feat(随机生成大数相加):

--- src/test/java/bytedance/ByteDance1Test.java
+++ src/test/java/bytedance/ByteDance1Test.java
@@ -1,6 +1,13 @@
 package bytedance;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.OptionalInt;
+import java.util.Random;
 
 /**
  * @author wangqimeng
@@ -15,18 +22,112 @@ public class ByteDance1Test {
     public static
     class Solution {
 
-        public int fun() {
+        Random uniqueRandom = new Random();
 
-            return 0;
+
+        public String generatorBitNumber(int bit) {
+            if (bit <= 0) {
+                return "";
+            }
+            char[] res = new char[bit];
+            res[0] = (char) (uniqueRandom.nextInt(9) + 1 + '0');
+
+            //除第一位是 1-9外,其余是0-9
+
+            for (int i = 1; i < bit; i++) {
+                res[i] = (char) (uniqueRandom.nextInt(10) + '0');
+            }
+            return new String(res);
+        }
+
+        public String generatorBitNumber(int start, int end) {
+            return generatorBitNumber(generatorBit(start, end));
+        }
+
+        public int generatorBit(int start, int end) {
+            return uniqueRandom.nextInt(end - start) + start;
+        }
+
+        public String sumLongNum(String[] nums) {
+            StringBuilder res = new StringBuilder();
+            OptionalInt maxOpt = Arrays.stream(nums).map(String::length).mapToInt(Integer::intValue).max();
+            if (!maxOpt.isPresent()) {
+                return "0";
+            }
+            int len = maxOpt.getAsInt();
+            int carry = 0;
+            for (int i = 0; i < len; i++) {
+                int sum = carry;
+                for (String num : nums) {
+                    int currLen = num.length();
+                    int v = currLen - i - 1;
+                    if (currLen - i - 1 < 0) {
+                        continue;
+                    }
+                    sum += num.charAt(v) - '0';
+                }
+                carry = sum / 10;
+                res.append(sum % 10);
+            }
+            //carry处理
+            while (carry != 0) {
+                res.append(carry % 10);
+                carry = carry / 10;
+            }
+
+            return res.reverse().toString();
+        }
+
+        public String[] generatorRandomNums(int N, int start, int end) {
+            String[] result = new String[N];
+            for (int i = 0; i < N; i++) {
+                result[i] = generatorBitNumber(start, end);
+            }
+            return result;
         }
     }
 
     public static class TestClass {
+        int N = 100;
+        int lenStart = 100;
+        int lenEnd = 200;
+        private Solution solution;
+
+        @Before
+        public void init() {
+            solution = new Solution();
+        }
+
+        @Test
+        public void generatorBitTest() {
+            for (int i = 0; i < 1000; i++) {
+                int bit = solution.generatorBit(lenStart, lenEnd);
+                Assert.assertTrue(bit >= lenStart && bit <= lenEnd);
+            }
+        }
+
+        @Test
+        public void generatorBitNumberTest() {
+            for (int i = 0; i < 1000; i++) {
+                String temp = solution.generatorBitNumber(lenStart, lenEnd);
+                Assert.assertTrue(temp.length() >= lenStart && temp.length() <= lenEnd);
+                Assert.assertTrue(temp.charAt(0) != '0');
+            }
+        }
+
 
         @Test
         public void defaultSolutionTest() {
-            Solution solution = new Solution();
+            String[] nums = solution.generatorRandomNums(N, lenStart, lenEnd);
+            Assert.assertEquals(calcSum(nums), solution.sumLongNum(nums));
+        }
 
+        private String calcSum(String[] nums) {
+            BigDecimal result = Arrays.stream(nums)
+                    .map(BigDecimal::new)
+                    .reduce(BigDecimal::add)
+                    .orElseThrow(() -> new RuntimeException("计算错误"));
+            return result.toString();
         }
     }
 
