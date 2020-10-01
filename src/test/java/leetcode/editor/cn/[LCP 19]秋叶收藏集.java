c485f928d54c351ab287777a46472c1f2c8e5caf
boommanpro <boommanpro@gmail.com>
2020-10-01 11:09:35 +0800

    feat(add problem [LCP 19]秋叶收藏集.java): [LCP 19]秋叶收藏集.java

---
+++ src/test/java/leetcode/editor/cn/[LCP 19]秋叶收藏集.java
@@ -0,0 +1,50 @@
+package leetcode.editor.cn;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+class SolutionTestLCP_19 {
+//小扣出去秋游，途中收集了一些红叶和黄叶，他利用这些叶子初步整理了一份秋叶收藏集 `leaves`， 字符串 `leaves` 仅包含小写字符 `r` 和 `
+//y`， 其中字符 `r` 表示一片红叶，字符 `y` 表示一片黄叶。
+//出于美观整齐的考虑，小扣想要将收藏集中树叶的排列调整成「红、黄、红」三部分。每部分树叶数量可以不相等，但均需大于等于 1。每次调整操作，小扣可以将一片红叶替
+//换成黄叶或者将一片黄叶替换成红叶。请问小扣最少需要多少次调整操作才能将秋叶收藏集调整完毕。
+//
+//**示例 1：**
+//>输入：`leaves = "rrryyyrryyyrr"`
+//>
+//>输出：`2`
+//>
+//>解释：调整两次，将中间的两片红叶替换成黄叶，得到 "rrryyyyyyyyrr"
+//
+//**示例 2：**
+//>输入：`leaves = "ryr"`
+//>
+//>输出：`0`
+//>
+//>解释：已符合要求，不需要额外操作
+//
+//**提示：**
+//- `3 <= leaves.length <= 10^5`
+//- `leaves` 中只包含字符 `'r'` 和字符 `'y'` 👍 68 👎 0
+
+    public static
+            //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+
+        public int minimumOperations(String leaves) {
+            return 0;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+    //Do some Test
+    public static class TestClass {
+
+        @Test
+        public void defaultSolutionTest() {
+            Solution solution = new Solution();
+            Assert.assertEquals(2, solution.minimumOperations("rrryyyrryyyrr"));
+            Assert.assertEquals(0, solution.minimumOperations("ryr"));
+        }
+    }
+}
