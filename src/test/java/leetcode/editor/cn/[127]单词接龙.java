b0c155e6ea1b45a79d830cb206a9935a35d6839a
boommanpro <boommanpro@gmail.com>
2020-11-05 19:06:26 +0800

    feat(solve [127]单词接龙):

---
+++ src/test/java/leetcode/editor/cn/[127]单词接龙.java
@@ -0,0 +1,132 @@
+package leetcode.editor.cn;
+
+import java.util.*;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+class SolutionTest127 {
+//给定两个单词（beginWord 和 endWord）和一个字典，找到从 beginWord 到 endWord 的最短转换序列的长度。转换需遵循如下规则：
+// 
+//
+// 
+// 每次转换只能改变一个字母。 
+// 转换过程中的中间单词必须是字典中的单词。 
+// 
+//
+// 说明: 
+//
+// 
+// 如果不存在这样的转换序列，返回 0。 
+// 所有单词具有相同的长度。 
+// 所有单词只由小写字母组成。 
+// 字典中不存在重复的单词。 
+// 你可以假设 beginWord 和 endWord 是非空的，且二者不相同。 
+// 
+//
+// 示例 1: 
+//
+// 输入:
+//beginWord = "hit",
+//endWord = "cog",
+//wordList = ["hot","dot","dog","lot","log","cog"]
+//
+//输出: 5
+//
+//解释: 一个最短转换序列是 "hit" -> "hot" -> "dot" -> "dog" -> "cog",
+//     返回它的长度 5。
+// 
+//
+// 示例 2: 
+//
+// 输入:
+//beginWord = "hit"
+//endWord = "cog"
+//wordList = ["hot","dot","dog","lot","log"]
+//
+//输出: 0
+//
+//解释: endWord "cog" 不在字典中，所以无法进行转换。 
+// Related Topics 广度优先搜索 
+// 👍 581 👎 0
+
+    public static
+            //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+
+        Map<String, Integer> wordId;
+
+        List<List<Integer>> edge;
+
+        int nodeNum;
+
+        public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+            wordId = new HashMap<>();
+            edge = new ArrayList<>();
+            nodeNum = 0;
+            for (String word : wordList) {
+                addEdge(word);
+            }
+            addEdge(beginWord);
+            if (!wordId.containsKey(endWord)) {
+                return 0;
+            }
+            int[] dis = new int[nodeNum];
+            Arrays.fill(dis, Integer.MAX_VALUE);
+            int beginId = wordId.get(beginWord), endId = wordId.get(endWord);
+            dis[beginId] = 0;
+
+            Queue<Integer> que = new LinkedList<Integer>();
+            que.offer(beginId);
+            while (!que.isEmpty()) {
+                int x = que.poll();
+                if (x == endId) {
+                    return dis[endId] / 2 + 1;
+                }
+                for (int it : edge.get(x)) {
+                    if (dis[it] == Integer.MAX_VALUE) {
+                        dis[it] = dis[x] + 1;
+                        que.offer(it);
+                    }
+                }
+            }
+            return 0;
+        }
+
+        public void addEdge(String word) {
+            addWord(word);
+            int id1 = wordId.get(word);
+            char[] array = word.toCharArray();
+            int length = array.length;
+            for (int i = 0; i < length; ++i) {
+                char tmp = array[i];
+                array[i] = '*';
+                String newWord = new String(array);
+                addWord(newWord);
+                int id2 = wordId.get(newWord);
+                edge.get(id1).add(id2);
+                edge.get(id2).add(id1);
+                array[i] = tmp;
+            }
+        }
+
+        public void addWord(String word) {
+            if (!wordId.containsKey(word)) {
+                wordId.put(word, nodeNum++);
+                edge.add(new ArrayList<>());
+            }
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+    //Do some Test
+    public static class TestClass {
+
+        @Test
+        public void defaultSolutionTest() {
+            Solution solution = new Solution();
+            Assert.assertEquals(5, solution.ladderLength("hit", "cog", Arrays.asList("hot", "dot", "dog", "lot", "log", "cog")));
+            Assert.assertEquals(0, solution.ladderLength("hit", "cog", Arrays.asList("hot", "dot", "dog", "lot", "log")));
+        }
+    }
+}
