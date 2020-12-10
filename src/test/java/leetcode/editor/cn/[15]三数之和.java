3f3350943bc9933aac163b8fcb045788ed985613
boommanpro <boommanpro@gmail.com>
2020-12-10 19:15:47 +0800

    feat(solve [15]三数之和):

---
+++ src/test/java/leetcode/editor/cn/[15]三数之和.java
@@ -0,0 +1,82 @@
+package leetcode.editor.cn;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import leetcode.editor.cn.utils.ArrayUtils;
+import org.junit.Assert;
+import org.junit.Test;
+
+class SolutionTest15 {
+//给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？请你找出所有满足条件且不重复
+//的三元组。 
+//
+// 注意：答案中不可以包含重复的三元组。 
+//
+// 
+//
+// 示例： 
+//
+// 给定数组 nums = [-1, 0, 1, 2, -1, -4]，
+//
+//满足要求的三元组集合为：
+//[
+//  [-1, 0, 1],
+//  [-1, -1, 2]
+//]
+// 
+// Related Topics 数组 双指针 
+// 👍 2808 👎 0
+
+    public static
+            //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public List<List<Integer>> threeSum(int[] nums) {
+            int n = nums.length;
+            Arrays.sort(nums);
+            List<List<Integer>> ans = new ArrayList<>();
+            // 枚举 a
+            for (int first = 0; first < n; ++first) {
+                // 需要和上一次枚举的数不相同
+                if (first > 0 && nums[first] == nums[first - 1]) {
+                    continue;
+                }
+                // c 对应的指针初始指向数组的最右端
+                int third = n - 1;
+                int target = -nums[first];
+                // 枚举 b
+                for (int second = first + 1; second < n; ++second) {
+                    // 需要和上一次枚举的数不相同
+                    if (second > first + 1 && nums[second] == nums[second - 1]) {
+                        continue;
+                    }
+                    // 需要保证 b 的指针在 c 的指针的左侧
+                    while (second < third && nums[second] + nums[third] > target) {
+                        --third;
+                    }
+                    // 如果指针重合，随着 b 后续的增加
+                    // 就不会有满足 a+b+c=0 并且 b<c 的 c 了，可以退出循环
+                    if (second == third) {
+                        break;
+                    }
+                    if (nums[second] + nums[third] == target) {
+                        ans.add(Arrays.asList(nums[first],nums[second],nums[third]));
+                    }
+                }
+            }
+            return ans;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+    //Do some Test
+    public static class TestClass {
+
+        @Test
+        public void defaultSolutionTest() {
+            Solution solution = new Solution();
+            Assert.assertEquals("[[-1, -1, 2], [-1, 0, 1]]", ArrayUtils.twoDimensionCollections2String(solution.threeSum(new int[]{-1, 0, 1, 2, -1, -4})));
+        }
+    }
+}
