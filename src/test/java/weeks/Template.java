f7f7446ac3c7489254754210412547720d348caa
boommanpro <boommanpro@gmail.com>
2021-01-17 10:23:26 +0800

    feat(add weeks template): 添加leetcode周赛练习模板

---
+++ src/test/java/weeks/Template.java
@@ -0,0 +1,40 @@
+package weeks;
+
+import org.junit.Test;
+
+/**
+ * 周赛练习模板
+ *
+ * @author <a href="mailto:boommanpro@gmail.com">boommanpro</a>
+ * @date 2021/1/17 10:21
+ * @created by boommanpro
+ */
+public class Template {
+
+
+    public static
+            //leetcode submit region begin(Prohibit modification and deletion)
+
+
+    class Solution {
+
+        public void fun() {
+
+        }
+    }
+
+
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+
+
+    //Do some Test
+    public static class TestClass {
+
+        @Test
+        public void defaultSolutionTest() {
+            Solution solution = new Solution();
+        }
+    }
+}
