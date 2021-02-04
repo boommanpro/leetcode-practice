fbe4b55fc29b10ee3f4dd3f75050e53b9468f47f
boommanpro <boommanpro@gmail.com>
2021-02-04 09:49:41 +0800

    feat(solve [643]子数组最大平均数 I): tag: slidewindow

--- src/test/java/leetcode/editor/cn/[643]子数组最大平均数 I.java
+++ src/test/java/leetcode/editor/cn/[643]子数组最大平均数 I.java
@@ -2,6 +2,7 @@ package leetcode.editor.cn;
 
 import org.junit.Assert;
 import org.junit.Test;
+
 class SolutionTest643 {
 //给定 n 个整数，找出平均数最大且长度为 k 的连续子数组，并输出该最大平均数。 
 //
@@ -27,34 +28,34 @@ class SolutionTest643 {
 // 👍 140 👎 0
 
     public static
-    //leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
-    public double findMaxAverage(int[] nums, int k) {
-        if (k > nums.length) {
-            return -1;
+            //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public double findMaxAverage(int[] nums, int k) {
+            if (k > nums.length) {
+                return -1;
+            }
+            long sum = 0;
+            for (int i = 0; i < k; i++) {
+                sum += nums[i];
+            }
+            double ans = sum / (double) k;
+            for (int i = 1; i < nums.length - k + 1; i++) {
+                sum -= nums[i - 1];
+                sum += nums[i + k - 1];
+                ans = Math.max(ans, sum / (double) k);
+            }
+            return ans;
         }
-        long sum = 0;
-        for (int i = 0; i < k; i++) {
-            sum += nums[i];
-        }
-        double ans = sum / (double) k;
-        for (int i = 1; i < nums.length - k + 1; i++) {
-            sum -= nums[i - 1];
-            sum += nums[i + k - 1];
-            ans = Math.max(ans, sum / (double) k);
-        }
-        return ans;
     }
-}
 //leetcode submit region end(Prohibit modification and deletion)
 
-//Do some Test
+    //Do some Test
     public static class TestClass {
 
         @Test
         public void defaultSolutionTest() {
             Solution solution = new Solution();
-            Assert.assertEquals(12.75, solution.findMaxAverage(new int[]{1, 12, -5, -6, 50, 3}, 4),0.0001);
+            Assert.assertEquals(12.75, solution.findMaxAverage(new int[]{1, 12, -5, -6, 50, 3}, 4), 0.0001);
         }
     }
 }
