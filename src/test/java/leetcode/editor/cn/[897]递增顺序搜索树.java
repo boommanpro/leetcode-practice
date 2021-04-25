0ebe122048c98d1d69ebd4f51551933daaa6eaef
boommanpro <boommanpro@gmail.com>
2021-04-25 10:45:27 +0800

    test(update [897]递增顺序搜索树): tag: 递归

--- src/test/java/leetcode/editor/cn/[897]递增顺序搜索树.java
+++ src/test/java/leetcode/editor/cn/[897]递增顺序搜索树.java
@@ -2,53 +2,39 @@ package leetcode.editor.cn;
 
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 class SolutionTest897 {
-//给你一个树，请你 按中序遍历 重新排列树，使树中最左边的结点现在是树的根，并且每个结点没有左子结点，只有一个右子结点。 
+//给你一棵二叉搜索树，请你 按中序遍历 将其重新排列为一棵递增顺序搜索树，使树中最左边的节点成为树的根节点，并且每个节点没有左子节点，只有一个右子节点。 
 //
 // 
 //
-// 示例 ： 
+// 示例 1： 
 //
-// 输入：[5,3,6,2,4,null,8,1,null,null,null,7,9]
-//
-//       5
-//      / \
-//    3    6
-//   / \    \
-//  2   4    8
-// /        / \ 
-//1        7   9
-//
+// 
+//输入：root = [5,3,6,2,4,null,8,1,null,null,null,7,9]
 //输出：[1,null,2,null,3,null,4,null,5,null,6,null,7,null,8,null,9]
+// 
 //
-// 1
-//  \
-//   2
-//    \
-//     3
-//      \
-//       4
-//        \
-//         5
-//          \
-//           6
-//            \
-//             7
-//              \
-//               8
-//                \
-//                 9  
+// 示例 2： 
+//
+// 
+//输入：root = [5,1,7]
+//输出：[1,null,5,null,7]
+// 
 //
 // 
 //
 // 提示： 
 //
 // 
-// 给定树中的结点数介于 1 和 100 之间。 
-// 每个结点都有一个从 0 到 1000 范围内的唯一整数值。 
+// 树中节点数的取值范围是 [1, 100] 
+// 0 <= Node.val <= 1000 
 // 
 // Related Topics 树 深度优先搜索 递归 
-// 👍 131 👎 0
+// 👍 175 👎 0
 
     public static
             //leetcode submit region begin(Prohibit modification and deletion)
@@ -89,13 +75,14 @@ class SolutionTest897 {
     }
 //leetcode submit region end(Prohibit modification and deletion)
 
-//Do some Test
+    //Do some Test
     public static class TestClass {
 
         @Test
         public void defaultSolutionTest() {
             Solution solution = new Solution();
-            Assert.assertEquals("[1, null, 2, null, 3, null, 4]",solution.increasingBST(TreeNode.getTreeNode(new Integer[]{2,1,4,null,null,3})).toIntArrayString());
+            Assert.assertEquals("[1, null, 2, null, 3, null, 4, null, 5, null, 6, null, 7, null, 8, null, 9]", solution.increasingBST(TreeNode.getTreeNode(new Integer[]{5, 3, 6, 2, 4, null, 8, 1, null, null, null, 7, 9})).toIntArrayString());
         }
+
     }
 }
