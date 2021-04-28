0abf35549bb653529179d40c838a4ced97fc7f15
boommanpro <boommanpro@gmail.com>
2021-04-28 10:13:27 +0800

    test(solve [633]平方数之和): tag: 枚举

---
+++ src/test/java/leetcode/editor/cn/[633]平方数之和.java
@@ -0,0 +1,76 @@
+package leetcode.editor.cn;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+class SolutionTest633 {
+//给定一个非负整数 c ，你要判断是否存在两个整数 a 和 b，使得 a2 + b2 = c 。 
+//
+// 
+//
+// 示例 1： 
+//
+// 输入：c = 5
+//输出：true
+//解释：1 * 1 + 2 * 2 = 5
+// 
+//
+// 示例 2： 
+//
+// 输入：c = 3
+//输出：false
+// 
+//
+// 示例 3： 
+//
+// 输入：c = 4
+//输出：true
+// 
+//
+// 示例 4： 
+//
+// 输入：c = 2
+//输出：true
+// 
+//
+// 示例 5： 
+//
+// 输入：c = 1
+//输出：true 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 0 <= c <= 231 - 1 
+// 
+// Related Topics 数学 
+// 👍 203 👎 0
+
+    public static
+            //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public boolean judgeSquareSum(int c) {
+            for(long a=0;a*a<=c;a++){
+                double b=Math.sqrt(c-a*a);
+                if (b==(int)b){
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+    //Do some Test
+    public static class TestClass {
+
+        @Test
+        public void defaultSolutionTest() {
+            Solution solution = new Solution();
+            Assert.assertTrue(solution.judgeSquareSum(200));
+        }
+
+    }
+}
