f83e1abf6ec5cbc0ca498035897bede1ac0453e0
boommanpro <boommanpro@gmail.com>
2022-05-17 00:21:25 +0800

    feat(solve [953]验证外星语词典):

---
+++ src/test/java/leetcode/editor/cn/[953]验证外星语词典.java
@@ -0,0 +1,97 @@
+package leetcode.editor.cn;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+class SolutionTest953 {
+//某种外星语也使用英文小写字母，但可能顺序 order 不同。字母表的顺序（order）是一些小写字母的排列。 
+//
+// 给定一组用外星语书写的单词 words，以及其字母表的顺序 order，只有当给定的单词在这种外星语中按字典序排列时，返回 true；否则，返回 
+//false。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：words = ["hello","leetcode"], order = "hlabcdefgijkmnopqrstuvwxyz"
+//输出：true
+//解释：在该语言的字母表中，'h' 位于 'l' 之前，所以单词序列是按字典序排列的。 
+//
+// 示例 2： 
+//
+// 
+//输入：words = ["word","world","row"], order = "worldabcefghijkmnpqstuvxyz"
+//输出：false
+//解释：在该语言的字母表中，'d' 位于 'l' 之后，那么 words[0] > words[1]，因此单词序列不是按字典序排列的。 
+//
+// 示例 3： 
+//
+// 
+//输入：words = ["apple","app"], order = "abcdefghijklmnopqrstuvwxyz"
+//输出：false
+//解释：当前三个字符 "app" 匹配时，第二个字符串相对短一些，然后根据词典编纂规则 "apple" > "app"，因为 'l' > '∅'，其中 '∅
+//' 是空白字符，定义为比任何其他字符都小（更多信息）。
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= words.length <= 100 
+// 1 <= words[i].length <= 20 
+// order.length == 26 
+// 在 words[i] 和 order 中的所有字符都是英文小写字母。 
+// 
+// Related Topics 数组 哈希表 字符串 👍 132 👎 0
+
+    public static
+            //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public boolean isAlienSorted(String[] words, String order) {
+            Map<Character, Integer> dict = new HashMap<>();
+            for (int i = 0; i < order.length(); i++) {
+                dict.put(order.charAt(i), i);
+            }
+            String prefix = "";
+            for (String word : words) {
+                if (!compareByDict(prefix, word, dict)) {
+                    return false;
+                }
+                prefix = word;
+            }
+            return true;
+        }
+
+        private boolean compareByDict(String prefix, String word, Map<Character, Integer> dict) {
+            int len = Math.min(prefix.length(), word.length());
+            for (int i = 0; i < len; i++) {
+                char A = prefix.charAt(i);
+                char B = word.charAt(i);
+                if (A == B) {
+                    continue;
+                }
+                return dict.get(A) < dict.get(B);
+            }
+            return prefix.length() <= word.length();
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+    //Do some Test
+    public static class TestClass {
+
+        @Test
+        public void defaultSolutionTest() {
+            Solution solution = new Solution();
+            Assert.assertTrue(solution.isAlienSorted(new String[]{"hello","leetcode"},"hlabcdefgijkmnopqrstuvwxyz"));
+            Assert.assertFalse(solution.isAlienSorted(new String[]{"apple","app"},"abcdefghijklmnopqrstuvwxyz"));
+        }
+
+    }
+}
