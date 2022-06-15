d0f19debcb69f73c23d40d517c47000413c3f630
boommanpro <boommanpro@gmail.com>
2022-06-16 00:15:16 +0800

    feat(solve [532]数组中的 k-diff 数对):

---
+++ src/test/java/leetcode/editor/cn/[532]数组中的 k-diff 数对.java
@@ -0,0 +1,85 @@
+package leetcode.editor.cn;
+
+import org.junit.Test;
+
+class SolutionTest532 {
+//给定一个整数数组和一个整数 k，你需要在数组里找到 不同的 k-diff 数对，并返回不同的 k-diff 数对 的数目。 
+//
+// 这里将 k-diff 数对定义为一个整数对 (nums[i], nums[j])，并满足下述全部条件： 
+//
+// 
+// 0 <= i < j < nums.length 
+// |nums[i] - nums[j]| == k 
+// 
+//
+// 注意，|val| 表示 val 的绝对值。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：nums = [3, 1, 4, 1, 5], k = 2
+//输出：2
+//解释：数组中有两个 2-diff 数对, (1, 3) 和 (3, 5)。
+//尽管数组中有两个1，但我们只应返回不同的数对的数量。
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：nums = [1, 2, 3, 4, 5], k = 1
+//输出：4
+//解释：数组中有四个 1-diff 数对, (1, 2), (2, 3), (3, 4) 和 (4, 5)。
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：nums = [1, 3, 1, 5, 4], k = 0
+//输出：1
+//解释：数组中只有一个 0-diff 数对，(1, 1)。
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= nums.length <= 10⁴ 
+// -10⁷ <= nums[i] <= 10⁷ 
+// 0 <= k <= 10⁷ 
+// 
+// Related Topics 数组 哈希表 双指针 二分查找 排序 👍 166 👎 0
+
+    public static
+            //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int findPairs(int[] nums, int k) {
+            int cnt = 0;
+            Arrays.sort(nums);
+            Integer prev = -1;
+            for (int l = 0, r = 1; r < nums.length; r++) {
+                while (l < r && nums[r] - nums[l] > k) {
+                    l++;
+                }
+                if (r != l && nums[r] - nums[l] == k && (prev == -1 || prev != nums[r])) {
+                    cnt++;
+                    prev = nums[r];
+                }
+            }
+            return cnt;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+    //Do some Test
+    public static class TestClass {
+
+        @Test
+        public void defaultSolutionTest() {
+            Solution solution = new Solution();
+        }
+
+    }
+}
