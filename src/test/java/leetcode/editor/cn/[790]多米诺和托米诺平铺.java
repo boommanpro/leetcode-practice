56c6ebd7fa310cf4a598f613bfde5068bb58993b
boommanpro <boommanpro@gmail.com>
2022-11-12 13:13:16 +0800

    feat(solve [790]多米诺和托米诺平铺):

--- src/test/java/leetcode/editor/cn/[790]多米诺和托米诺平铺.java
+++ src/test/java/leetcode/editor/cn/[790]多米诺和托米诺平铺.java
@@ -47,23 +47,15 @@ class SolutionTest790 {
         private static final int MOD = (int) 1e9 + 7;
 
         public int numTilings(int n) {
-            if (n == 1) {
-                return 1;
+            int[][] dp = new int[n + 1][4];
+            dp[0][3] = 1;
+            for (int i = 1; i <= n; i++) {
+                dp[i][0] = dp[i - 1][3];
+                dp[i][1] = (dp[i - 1][0] + dp[i - 1][2]) % MOD;
+                dp[i][2] = (dp[i - 1][0] + dp[i - 1][1]) % MOD;
+                dp[i][3] = (((dp[i - 1][0] + dp[i - 1][1]) % MOD + dp[i - 1][2]) % MOD + dp[i - 1][3]) % MOD;
             }
-            if (n == 2) {
-                return 2;
-            }
-            if (n == 3) {
-                return 5;
-            }
-            int[] dp = new int[n + 1];
-            dp[1] = 1;
-            dp[2] = 2;
-            dp[3] = 5;
-            for (int i = 4; i <= n; i++) {
-                dp[i] = ((2 * dp[i - 1] % MOD) + dp[i - 3]) % MOD;
-            }
-            return dp[n];
+            return dp[n][3];
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
