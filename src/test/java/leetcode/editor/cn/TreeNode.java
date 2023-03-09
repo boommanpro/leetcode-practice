fac5c0823365aa3e5f51ab207f3aa71001fedbec
boommanpro <boommanpro@gmail.com>
2023-03-09 23:00:40 +0800

    feat(solve [2379]得到 K 个黑块的最少涂色次数):

--- src/test/java/leetcode/editor/cn/TreeNode.java
+++ src/test/java/leetcode/editor/cn/TreeNode.java
@@ -11,13 +11,13 @@ import lombok.Data;
 @Data
 public class TreeNode {
 
-    int val;
+    public int val;
 
-    TreeNode left;
+   public TreeNode left;
 
-    TreeNode right;
+    public TreeNode right;
 
-    TreeNode(int x) {
+    public  TreeNode(int x) {
         val = x;
     }
 
