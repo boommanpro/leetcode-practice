396a962320cc9bf58fb2b73d3671a048d18e42da
boommanpro <boommanpro@gmail.com>
2023-11-04 11:59:10 +0800

    feat(solve [421]数组中两个数的最大异或值):

--- src/test/java/leetcode/editor/cn/[421]数组中两个数的最大异或值.java
+++ src/test/java/leetcode/editor/cn/[421]数组中两个数的最大异或值.java
@@ -1,26 +1,29 @@
 package leetcode.editor.cn;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.junit.Assert;
 import org.junit.Test;
 
 class SolutionTest421 {
-//给定一个非空数组，数组中元素为 a0, a1, a2, … , an-1，其中 0 ≤ ai < 231 。 
+//给定一个非空数组，数组中元素为 a0, a1, a2, … , an-1，其中 0 ≤ ai < 231 。
 //
-// 找到 ai 和aj 最大的异或 (XOR) 运算结果，其中0 ≤ i, j < n 。 
+// 找到 ai 和aj 最大的异或 (XOR) 运算结果，其中0 ≤ i, j < n 。
 //
-// 你能在O(n)的时间解决这个问题吗？ 
+// 你能在O(n)的时间解决这个问题吗？
 //
-// 示例: 
+// 示例:
 //
-// 
+//
 //输入: [3, 10, 5, 25, 2, 8]
 //
 //输出: 28
 //
 //解释: 最大的结果是 5 ^ 25 = 28.
-// 
+//
 // Related Topics 位运算 字典树
 
     public static
@@ -33,56 +36,24 @@ class SolutionTest421 {
          * 如果ab相同 ab异或结果为0
          */
         public int findMaximumXOR(int[] nums) {
-            // Compute length L of max number in a binary representation
-            int maxNum = nums[0];
-            for (int num : nums) maxNum = Math.max(maxNum, num);
-            int L = (Integer.toBinaryString(maxNum)).length();
-
-            // zero left-padding to ensure L bits for each number
-            int n = nums.length, bitmask = 1 << L;
-            String[] strNums = new String[n];
-            for (int i = 0; i < n; ++i) {
-                strNums[i] = Integer.toBinaryString(bitmask | nums[i]).substring(1);
-            }
-
-            TrieNode trie = new TrieNode();
-            int maxXor = 0;
-            for (String num : strNums) {
-                TrieNode node = trie, xorNode = trie;
-                int currXor = 0;
-                for (Character bit : num.toCharArray()) {
-                    // insert new number in trie
-                    if (node.children.containsKey(bit)) {
-                        node = node.children.get(bit);
-                    } else {
-                        TrieNode newNode = new TrieNode();
-                        node.children.put(bit, newNode);
-                        node = newNode;
-                    }
-
-                    // compute max xor of that new number
-                    // with all previously inserted
-                    Character toggledBit = bit == '1' ? '0' : '1';
-                    if (xorNode.children.containsKey(toggledBit)) {
-                        currXor = (currXor << 1) | 1;
-                        xorNode = xorNode.children.get(toggledBit);
-                    } else {
-                        currXor = currXor << 1;
-                        xorNode = xorNode.children.get(bit);
+            int max = Arrays.stream(nums).max().getAsInt();
+            int len = Integer.toBinaryString(max).length();
+            int ans = 0;
+            for (int k = len - 1; k >= 0; k--) {
+                Set<Integer> seen = new HashSet<>();
+                for (int num : nums) {
+                    seen.add(num >> k);
+                }
+                ans <<= 1;
+                int nextAns = ans + 1;
+                for (int num : nums) {
+                    if (seen.contains((num >> k) ^ nextAns)) {
+                        ans = nextAns;
+                        break;
                     }
                 }
-                maxXor = Math.max(maxXor, currXor);
             }
-
-            return maxXor;
-        }
-
-        static class TrieNode {
-
-            HashMap<Character, TrieNode> children = new HashMap<>();
-
-            public TrieNode() {
-            }
+            return ans;
         }
 
     }
