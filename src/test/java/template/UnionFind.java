b0c0fac5712ef822accce2335ad8e21029f7ff25
boommanpro <boommanpro@gmail.com>
2023-11-25 22:08:30 +0800

    feat(修改为labuladong的模板):

--- src/test/java/template/UnionFind.java
+++ src/test/java/template/UnionFind.java
@@ -1,34 +1,49 @@
 package template;
 
 public class UnionFind {
-    int[] parents;
+    // 连通分量个数
+    private int count;
+    // 存储每个节点的父节点
+    private int[] parent;
 
+    // n 为图中节点的个数
     public UnionFind(int n) {
-        parents = new int[n];
+        this.count = n;
+        parent = new int[n];
         for (int i = 0; i < n; i++) {
-            parents[i] = i;
+            parent[i] = i;
         }
     }
 
-    public boolean union(int x, int y) {
-        int px = find(x);
-        int py = find(y);
-        if (px == py) {
-            return false;
-        }
-        if (px > py) {
-            int temp = py;
-            py = px;
-            px = temp;
-        }
-        parents[px] = py;
-        return true;
+    // 将节点 p 和节点 q 连通
+    public void union(int p, int q) {
+        int rootP = find(p);
+        int rootQ = find(q);
+
+        if (rootP == rootQ)
+            return;
+
+        parent[rootQ] = rootP;
+        // 两个连通分量合并成一个连通分量
+        count--;
     }
 
-    private int find(int v) {
-        if (v != parents[v]) {
-            parents[v] = find(parents[v]);
+    // 判断节点 p 和节点 q 是否连通
+    public boolean connected(int p, int q) {
+        int rootP = find(p);
+        int rootQ = find(q);
+        return rootP == rootQ;
+    }
+
+    public int find(int x) {
+        if (parent[x] != x) {
+            parent[x] = find(parent[x]);
         }
-        return parents[v];
+        return parent[x];
+    }
+
+    // 返回图中的连通分量个数
+    public int count() {
+        return count;
     }
 }
