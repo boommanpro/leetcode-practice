36bf705bd73abda882b4ff6700626cd148d9fb80
boommanpro <boommanpro@gmail.com>
2023-12-21 22:02:54 +0800

    feat(add 单调栈):

---
+++ src/test/java/template/MonotonicStack.java
@@ -0,0 +1,11 @@
+package template;
+
+import org.junit.Test;
+
+public class MonotonicStack {
+    @Test
+    public void test() {
+        //单调栈总结
+        //对于前缀和场景，需要考虑是否需要进行计数，需要手撸模拟求和情况
+    }
+}
