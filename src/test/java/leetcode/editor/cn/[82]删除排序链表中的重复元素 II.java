ef06f312ce3110d4ab9a171b55df273359400ee3
boommanpro <boommanpro@gmail.com>
2024-01-15 22:20:00 +0800

    feat(solve [82]删除排序链表中的重复元素 II):

--- src/test/java/leetcode/editor/cn/[82]删除排序链表中的重复元素 II.java
+++ src/test/java/leetcode/editor/cn/[82]删除排序链表中的重复元素 II.java
@@ -7,16 +7,16 @@ class SolutionTest82 {
 
     //给定一个排序链表，删除所有含有重复数字的节点，只保留原始链表中 没有重复出现 的数字。
 //
-// 示例 1: 
+// 示例 1:
 //
 // 输入: 1->2->3->3->4->4->5
 //输出: 1->2->5
-// 
 //
-// 示例 2: 
+//
+// 示例 2:
 //
 // 输入: 1->1->1->2->3
-//输出: 2->3 
+//输出: 2->3
 // Related Topics 链表
     public static
 
@@ -32,29 +32,19 @@ class SolutionTest82 {
     class Solution {
 
         public ListNode deleteDuplicates(ListNode head) {
-            if (head == null || head.next == null) {
-                return head;
-            }
             ListNode dummy = new ListNode(-1);
             dummy.next = head;
-            //
-            ListNode prev = dummy;
-            ListNode curr = head;
-            // -1     1->1->1->2
-            boolean deletable = false;
-            while (curr != null && curr.next != null) {
-                while (curr.next != null && curr.val == curr.next.val) {
+            ListNode curr = dummy;
+            while (curr.next != null && curr.next.next != null) {
+                if (curr.next.val == curr.next.next.val) {
+                    int value = curr.next.val;
+                    while (curr.next.next != null && curr.next.next.val == value) {
+                        curr.next.next = curr.next.next.next;
+                    }
                     curr.next = curr.next.next;
-                    deletable = true;
-                }
-                if (deletable) {
-                    prev.next = curr.next;
-                    deletable = false;
                 } else {
-                    prev = curr;
+                    curr = curr.next;
                 }
-                curr = curr.next;
-
             }
 
             return dummy.next;
@@ -70,11 +60,11 @@ class SolutionTest82 {
             Solution solution = new Solution();
             ListNode node1 = new ListNode(1);
             node1.addNext(2).addNext(3).addNext(3).addNext(4).addNext(4).addNext(5);
-            Assert.assertEquals("125", solution.deleteDuplicates(node1).getPositiveListNodeValue());
+            Assert.assertEquals("[1, 2, 5]", solution.deleteDuplicates(node1).getPositiveListNodeValue());
 
             ListNode node2 = new ListNode(1);
             node2.addNext(1).addNext(1).addNext(2).addNext(3);
-            Assert.assertEquals("23", solution.deleteDuplicates(node2).getPositiveListNodeValue());
+            Assert.assertEquals("[2, 3]", solution.deleteDuplicates(node2).getPositiveListNodeValue());
 
             ListNode node3 = new ListNode(1);
             node3.addNext(1);
