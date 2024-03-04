3029f14afb5be35e1fc441559bbf388e8b685cc5
boommanpro <boommanpro@gmail.com>
2024-03-04 21:43:15 +0800

    feat(solve [232]用栈实现队列):

--- src/test/java/leetcode/editor/cn/[232]用栈实现队列.java
+++ src/test/java/leetcode/editor/cn/[232]用栈实现队列.java
@@ -6,96 +6,92 @@ import org.junit.Test;
 import java.util.Stack;
 
 class SolutionTest232 {
-//使用栈实现队列的下列操作： 
+//使用栈实现队列的下列操作：
 //
-// 
-// push(x) -- 将一个元素放入队列的尾部。 
-// pop() -- 从队列首部移除元素。 
-// peek() -- 返回队列首部的元素。 
-// empty() -- 返回队列是否为空。 
-// 
 //
-// 
+// push(x) -- 将一个元素放入队列的尾部。
+// pop() -- 从队列首部移除元素。
+// peek() -- 返回队列首部的元素。
+// empty() -- 返回队列是否为空。
 //
-// 示例: 
+//
+//
+//
+// 示例:
 //
 // MyQueue queue = new MyQueue();
 //
 //queue.push(1);
-//queue.push(2);  
+//queue.push(2);
 //queue.peek();  // 返回 1
 //queue.pop();   // 返回 1
-//queue.empty(); // 返回 false 
+//queue.empty(); // 返回 false
 //
-// 
 //
-// 说明: 
 //
-// 
+// 说明:
+//
+//
 // 你只能使用标准的栈操作 -- 也就是只有 push to top, peek/pop from top, size, 和 is empty 操作是合法的。
-// 
-// 你所使用的语言也许不支持栈。你可以使用 list 或者 deque（双端队列）来模拟一个栈，只要是标准的栈操作即可。 
-// 假设所有操作都是有效的 （例如，一个空的队列不会调用 pop 或者 peek 操作）。 
-// 
+//
+// 你所使用的语言也许不支持栈。你可以使用 list 或者 deque（双端队列）来模拟一个栈，只要是标准的栈操作即可。
+// 假设所有操作都是有效的 （例如，一个空的队列不会调用 pop 或者 peek 操作）。
+//
 // Related Topics 栈 设计
 
     public static
             //leetcode submit region begin(Prohibit modification and deletion)
     class MyQueue {
 
-        /**
-         * 用来存储 入栈
-         */
-        private final Stack<Integer> s1;
-
-        /**
-         * 用来存储 出栈
-         */
-        private final Stack<Integer> s2;
+        private Stack<Integer> stack1;
+        private Stack<Integer> stack2;
 
         /**
          * Initialize your data structure here.
          */
         public MyQueue() {
-            s1 = new Stack<>();
-            s2 = new Stack<>();
+            stack1 = new Stack<>();
+            stack2 = new Stack<>();
         }
 
         /**
          * Push element x to the back of queue.
          */
         public void push(int x) {
-            while (!s2.empty()) {
-                s1.push(s2.pop());
-            }
-            s1.push(x);
+            stack1.push(x);
         }
 
         /**
          * Removes the element from in front of queue and returns that element.
          */
         public int pop() {
-            while (!s1.empty()) {
-                s2.push(s1.pop());
-            }
-            return s2.pop();
+            transform();
+            return stack2.pop();
         }
+
+
 
         /**
          * Get the front element.
          */
         public int peek() {
-            while (!s1.empty()) {
-                s2.push(s1.pop());
-            }
-            return s2.peek();
+            transform();
+            return stack2.peek();
         }
 
         /**
          * Returns whether the queue is empty.
          */
         public boolean empty() {
-            return s1.empty() && s2.empty();
+            return stack1.isEmpty() && stack2.isEmpty();
+        }
+
+        private void transform() {
+            while (stack2.isEmpty() && !stack1.isEmpty()) {
+                while (!stack1.isEmpty()) {
+                    stack2.push(stack1.pop());
+                }
+            }
         }
     }
 
