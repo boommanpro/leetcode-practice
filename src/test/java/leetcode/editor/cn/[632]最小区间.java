7bc388e946a99b774a4c519332490f00ebff7452
boommanpro <boommanpro@gmail.com>
2024-11-24 13:59:19 +0800

    feat(solve [632]最小区间):

--- src/test/java/leetcode/editor/cn/[632]最小区间.java
+++ src/test/java/leetcode/editor/cn/[632]最小区间.java
@@ -2,87 +2,84 @@ package leetcode.editor.cn;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+
+import static org.junit.Assert.assertArrayEquals;
 
 class SolutionTest632 {
-//你有 k 个升序排列的整数数组。找到一个最小区间，使得 k 个列表中的每个列表至少有一个数包含在其中。 
+//你有 k 个升序排列的整数数组。找到一个最小区间，使得 k 个列表中的每个列表至少有一个数包含在其中。
 //
-// 我们定义如果 b-a < d-c 或者在 b-a == d-c 时 a < c，则区间 [a,b] 比 [c,d] 小。 
+// 我们定义如果 b-a < d-c 或者在 b-a == d-c 时 a < c，则区间 [a,b] 比 [c,d] 小。
 //
-// 示例 1: 
+// 示例 1:
 //
-// 
+//
 //输入:[[4,10,15,24,26], [0,9,12,20], [5,18,22,30]]
 //输出: [20,24]
-//解释: 
+//解释:
 //列表 1：[4, 10, 15, 24, 26]，24 在区间 [20,24] 中。
 //列表 2：[0, 9, 12, 20]，20 在区间 [20,24] 中。
 //列表 3：[5, 18, 22, 30]，22 在区间 [20,24] 中。
-// 
 //
-// 注意: 
 //
-// 
-// 给定的列表可能包含重复元素，所以在这里升序表示 >= 。 
-// 1 <= k <= 3500 
-// -105 <= 元素的值 <= 105 
-// 对于使用Java的用户，请注意传入类型已修改为List<List<Integer>>。重置代码模板后可以看到这项改动。 
-// 
+// 注意:
+//
+//
+// 给定的列表可能包含重复元素，所以在这里升序表示 >= 。
+// 1 <= k <= 3500
+// -105 <= 元素的值 <= 105
+// 对于使用Java的用户，请注意传入类型已修改为List<List<Integer>>。重置代码模板后可以看到这项改动。
+//
 // Related Topics 哈希表 双指针 字符串
 
     public static
             //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public int[] smallestRange(List<List<Integer>> nums) {
-            int size = nums.size();
-            Map<Integer, List<Integer>> indices = new HashMap<Integer, List<Integer>>();
-            int xMin = Integer.MAX_VALUE, xMax = Integer.MIN_VALUE;
-            for (int i = 0; i < size; i++) {
-                for (int x : nums.get(i)) {
-                    List<Integer> list = indices.getOrDefault(x, new ArrayList<Integer>());
-                    list.add(i);
-                    indices.put(x, list);
-                    xMin = Math.min(xMin, x);
-                    xMax = Math.max(xMax, x);
+            int k = nums.size();
+            Map<Integer, List<Integer>> index = new HashMap<>();
+            int min = Integer.MAX_VALUE;
+            int max = Integer.MIN_VALUE;
+            for (int i = 0; i < k; i++) {
+                for (Integer v : nums.get(i)) {
+                    index.computeIfAbsent(v, p -> new ArrayList<>()).add(i);
+                    min = Math.min(v, min);
+                    max = Math.max(v, max);
                 }
             }
-
-            int[] freq = new int[size];
-            int inside = 0;
-            int left = xMin, right = xMin - 1;
-            int bestLeft = xMin, bestRight = xMax;
-
-            while (right < xMax) {
-                right++;
-                if (indices.containsKey(right)) {
-                    for (int x : indices.get(right)) {
-                        freq[x]++;
-                        if (freq[x] == 1) {
-                            inside++;
+            int[] res = new int[]{min, max};
+            int[] freq = new int[k];
+            int cnt = 0;
+            int l = min, r = min - 1;
+            while (r < max) {
+                r++;
+                if (index.containsKey(r)) {
+                    for (int i : index.get(r)) {
+                        freq[i]++;
+                        if (freq[i] == 1) {
+                            cnt++;
                         }
                     }
-                    while (inside == size) {
-                        if (right - left < bestRight - bestLeft) {
-                            bestLeft = left;
-                            bestRight = right;
+                    while (cnt == k) {
+                        if (r - l < res[1] - res[0]) {
+                            res[0] = l;
+                            res[1] = r;
                         }
-                        if (indices.containsKey(left)) {
-                            for (int x : indices.get(left)) {
-                                freq[x]--;
-                                if (freq[x] == 0) {
-                                    inside--;
+
+                        if (index.containsKey(l)) {
+                            for (int i : index.get(l)) {
+                                freq[i]--;
+                                if (freq[i] == 0) {
+                                    cnt--;
                                 }
                             }
+
                         }
-                        left++;
+                        l++;
                     }
                 }
             }
-
-            return new int[]{bestLeft, bestRight};
+            return res;
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
@@ -93,6 +90,16 @@ class SolutionTest632 {
         @Test
         public void defaultSolutionTest() {
             Solution solution = new Solution();
+
+
+            // 基本测试用例
+            List<List<Integer>> input1 = Arrays.asList(
+                    Arrays.asList(4, 10, 15, 24, 26),
+                    Arrays.asList(0, 9, 12, 20),
+                    Arrays.asList(5, 18, 22, 30)
+            );
+            assertArrayEquals(new int[]{20, 24}, solution.smallestRange(input1));
+
         }
     }
 }
